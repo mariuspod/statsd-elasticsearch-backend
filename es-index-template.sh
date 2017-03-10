482090fc81608ca123c9692a27e6b722d26d2420
Marius Podwyszynski <marius.pod@gmail.com>
2017-03-10 17:33:44 +0100

    Allow to set HTTP Basic Auth for index templates.

--- es-index-template.sh
+++ es-index-template.sh
@@ -1,4 +1,4 @@
-curl -XPUT "${ES_HOST:-localhost}:${ES_PORT:-9200}/_template/statsd-template" -d '
+curl -XPUT "${ES_USERNAME:-user}:${ES_PASSWORD-pass}@${ES_HOST:-localhost}:${ES_PORT:-9200}/_template/statsd-template" -d '
 {
     "template" : "statsd-*",
     "settings" : {
